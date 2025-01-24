27d99142cc6463932b9b83ac049bdb271e0104b8
ZakL1 lyel <zakerylyel32@gmail.com>
2025-01-24 18:23:10 +0000

    set up post model

--- home/views.py
+++ home/views.py
@@ -9,3 +9,35 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "home/index.html"
     paginate_by = 6
+
+def post_detail(request, slug):
+    """
+    Display an individual :model:`blog.Post`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.Post`.
+
+    **Template:**
+
+    :template:`blog/post_detail.html`
+    """
+
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+    comments = post.comments.all().order_by("-created_on")
+    comment_count = post.comments.filter(approved=True).count()
+    if request.method == "POST":
+        print("Recieved a POST request")
+        comment_form = CommentForm(data=request.POST)
+        print("About to render template")
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Comment submitted and awaiting approval'
+    )
